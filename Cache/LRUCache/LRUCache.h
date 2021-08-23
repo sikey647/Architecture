752056fdda6a8eec2b11a30c41886eac95c0ee40
liuciqi <liuciqi@zuoyebang.com>
2021-08-23 11:12:00 +0800

    add lru cache

--- Cache/LRUCache/LRUCache.h
+++ Cache/LRUCache/LRUCache.h
@@ -14,8 +14,42 @@ struct Node {
 class LRUCache {
 public:
     LRUCache(int capacity);
-    int get(int key);
-    void put(int key, int value);
+
+    int get(int key) {
+        auto itr = m_map.find(key);
+        if (itr == m_map.end()) {
+            return -1;
+        }
+
+        auto nodeItr = itr->second;
+        int value = nodeItr->value;
+        m_list.erase(nodeItr);
+        m_list.emplace_front(key, value);
+        m_map[key] = m_list.begin();
+        return value;
+
+        return -1;
+    }
+
+    void put(int key, int value) {
+        auto itr = m_map.find(key);
+        if (itr != m_map.end()) { // 存在
+            // 更新
+            auto nodeItr = itr->second;
+            m_list.erase(nodeItr);
+            m_list.emplace_front(key, value);
+            m_map[key] = m_list.begin();
+        } else { // 不存在
+            if (m_list.size() >= m_capacity) { // 无空间
+                auto& backNode = m_list.back();
+                m_map.erase(backNode.key);
+                m_list.pop_back();
+            } 
+            m_list.emplace_front(key, value);
+            m_map[key] = m_list.begin();
+            m_capacity++;
+        }
+    }
 
 private:
     std::list<Node> m_list;
